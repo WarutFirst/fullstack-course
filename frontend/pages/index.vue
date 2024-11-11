c76bd2e9d5d0965dadf1723ad7580da5f853cc59
WarutFirst <firstwarut@gmail.com>
2024-11-11 16:10:15 +0700

    frontend-08-page-homepage

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,23 @@
 <script setup>
 // TODO: 1. เติม useHead
-// useHead({
-//     title: 'Pichet Itngam\'s portfolios',
-//     meta: [
-//         { name: 'description', content: 'Pichet Itgnam\'s portfolios' },
-//     ],
-// })
+useHead({
+    title: 'Pichet Itngam\'s portfolios',
+    meta: [
+        { name: 'description', content: 'Pichet Itgnam\'s portfolios' },
+    ],
+})
 </script>
 
 <template>
     <!-- TODO: 2. ใส่ placeholder สำหรับ แต่ละ section -->
-    <!-- <section class="mb-10"></section> -->
+    <section class="mb-10">
+        Skill List
+    </section>
+    <section class="mb-10">
+        Experience List
+    </section>
+    <section class="mb-10">
+        Portfolio List
+    </section>
+    
 </template>
